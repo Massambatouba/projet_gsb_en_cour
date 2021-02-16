e2fe52c811a31cf9f2907ff2c358eaf8bfdf7d4d
massambatouba <samndodia@gmail.com>
2021-02-16 22:43:09 +0100

    firt git

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 let p = document.querySelector("#fleche")
 let nom = document.querySelector(".nom");
-p.addEventListener("click", (evt) =>{
-    console.log(evt);
-    nom.style.display="inline-block";
-    p.style.display="none";
+let rapports;
+let rapport = document.querySelector("#listRapports");
+const url = "http://localhost:90/gsb/visiteur/3";
 
-   
-});
-nom.addEventListener("click", (evt) =>{
-    console.log(evt);
-    p.style.display="inline-block";
-    nom.style.display="none";
 
-   
+p.addEventListener("click", (evt) => {
+    console.log(evt);
+    nom.style.display = "inline-block";
+    p.style.display = "none";
+
 });
+nom.addEventListener("click", (evt) => {
+    console.log(evt);
+    p.style.display = "inline-block";
+    nom.style.display = "none";
+});
+
+
+
+
+fetch(url)                          // Le fetch va chercher l'instruction
+    .then(response => response.json())  // parse la réponse en json
+    .then((data) => {
+                        // récupère le tableau d'objets
+        // Générer le visuel
+        data.rapports.forEach(element => {
+
+            rapport.insertAdjacentHTML('beforeEnd',
+                `<li>
+                    <p>${element.id} </p>
+                    <p>${element.date}</p>
+                    <p>${element.bilan}</p>
+                    <p>${element.motif}</p>                    
+                </li >`);
+        });
+    })
+    .catch((error) => {                 // afficher une erreur
+        console.log(`Voici mon erreur ${error}`);
+    });
 
 
